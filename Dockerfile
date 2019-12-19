4768e779d716c69ca47775f3fbde31f2a57e4ef4
Renan <renan@grupocriar>
2019-12-19 15:43:52 -0300

    Adicionado cache e configurado o rabbitmq
    Took 10 minutes

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,7 @@ COPY build/bluerecords.jar bluerecords.jar
 ENV DATABASE_URL jdbc:h2:nio:./data/bluerecords_db;MODE=MySQL
 ENV DATABASE_USER root
 ENV DATABASE_PASSWORD 123456
-
+ENV RABBITMQ_HOST rabbitmq
+ENV RABBITMQ_USER rabbitmqUser
+ENV RABBITMQ_PASSWORD rabbitmqPass
 ENTRYPOINT ["java", "-Djava.security.egd=file:/dev/./urandom", "-Dspring.profiles.active=prod", "-jar", "/bluerecords.jar"]
--- application/dataprovider/src/main/kotlin/br/com/beblue/bluerecords/data/cashback/CashBackRepositorioImpl.kt
+++ application/dataprovider/src/main/kotlin/br/com/beblue/bluerecords/data/cashback/CashBackRepositorioImpl.kt
@@ -6,14 +6,16 @@ import br.com.beblue.bluerecords.core.repositorio.CashBackRepositorio
 import br.com.beblue.bluerecords.core.repositorio.GeneroRepositorio
 import br.com.beblue.bluerecords.data.cashback.mapper.CashBackRowMapper
 import br.com.beblue.bluerecords.data.genero.mapper.GeneroRowMapper
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.jdbc.core.JdbcTemplate
 
-class CashBackRepositorioImpl(private val jdbcTemplate: JdbcTemplate) : CashBackRepositorio {
+open class CashBackRepositorioImpl(private val jdbcTemplate: JdbcTemplate) : CashBackRepositorio {
 
     private val SELECT_CASHBACK = " SELECT id_cashback,id_genero,dia_semana,porcentagem_cash_back " +
             " FROM cashback_tbl" +
             " where id_genero = ? and dia_semana = ?; "
 
+    @Cacheable(value = ["buscarCashBack"], key="{#idGenero,#diaSemana}", cacheManager = "cacheManagerWithTenMinutes")
     override fun buscarCashBack(idGenero: Int, diaSemana: Int): CashBack {
         val cashback = jdbcTemplate.query(
             SELECT_CASHBACK,
